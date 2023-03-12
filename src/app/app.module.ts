4260586fab67c1f3b05135e7753b7016819fff1c
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2023-03-12 18:04:54 +0100

    install ServiceWorker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ReadonlyFieldComponent } from './readonly-field/readonly-field.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, ReadonlyFieldComponent],
@@ -26,6 +27,12 @@ import { ReadonlyFieldComponent } from './readonly-field/readonly-field.componen
     MatButtonModule,
     MatSliderModule,
     MatSnackBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
